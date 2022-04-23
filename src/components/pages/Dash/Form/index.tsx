3a40d59e66ccbaab212e1b66702a30be5bccaa52
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-04-23 19:06:27 -0300

    feat: created form to post new product

---
+++ src/components/pages/Dash/Form/index.tsx
@@ -0,0 +1,105 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button, TextField } from '@material-ui/core';
+import * as yup from 'yup';
+
+import { UseFood } from '../../../../hooks/Food';
+
+export const Form = () => {
+  const validateForm = yup.object({
+    name: yup.string().required('O nome é obrigatório!'),
+    description: yup
+      .string()
+      .required('A descrição é obrigatória!'),
+    price: yup.string().required('O preço é obrigatório!'),
+    thumbnail: yup
+      .string()
+      .required('A thumbnail é obrigatória!'),
+    category: yup
+      .string()
+      .required('A categoria é obrigatória!'),
+    brand: yup.string().required('A marca é obrigatória!'),
+    monthInstallment: yup
+      .string()
+      .required('Os meses de parcelamento é obrigatório!'),
+    quantity: yup
+      .string()
+      .required('A quantidade é obrigatória!'),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validateForm),
+  });
+
+  const { onsubmit } = UseFood();
+
+  return (
+    <form onSubmit={handleSubmit(onsubmit)}>
+      <TextField
+        variant="outlined"
+        name="name"
+        type="text"
+        label={errors.name?.message || 'Nome'}
+        {...register('name')}
+      />
+      <TextField
+        variant="outlined"
+        name="description"
+        type="text"
+        label={errors.description?.message || 'Descrição'}
+        {...register('description')}
+      />
+      <TextField
+        variant="outlined"
+        name="price"
+        type="number"
+        label={errors.price?.message || 'Price'}
+        {...register('price')}
+      />
+      <TextField
+        variant="outlined"
+        name="thumbnail"
+        type="text"
+        label={errors.thumbnail?.message || 'Thumbnail'}
+        {...register('thumbnail')}
+      />
+      <TextField
+        variant="outlined"
+        name="category"
+        type="text"
+        label={errors.category?.message || 'Categoria'}
+        {...register('category')}
+      />
+      <TextField
+        variant="outlined"
+        name="brand"
+        type="text"
+        label={errors.brand?.message || 'Marca'}
+        {...register('brand')}
+      />
+      <TextField
+        variant="outlined"
+        name="monthInstallment"
+        type="number"
+        label={
+          errors.monthInstallment?.message ||
+          'Parcelamento (meses)'
+        }
+        {...register('monthInstallment')}
+      />
+      <TextField
+        variant="outlined"
+        name="quantity"
+        type="number"
+        label={errors.quantity?.message || 'Quantidade'}
+        {...register('quantity')}
+      />
+      <Button variant="contained" type="submit">
+        Postar
+      </Button>
+    </form>
+  );
+};
