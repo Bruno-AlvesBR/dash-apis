66833872227b705b82b8082de1d5fb8aa7811a99
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-04-23 18:58:34 -0300

    feat: create card content to be calling in project

---
+++ src/components/core/CardContent/index.tsx
@@ -0,0 +1,67 @@
+import { Button } from '@material-ui/core';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { memo, useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { Skeleton } from '@mui/material';
+
+import { IFoodProps } from '../../../interfaces/IFoodsProps';
+
+import { Card, Title, Description } from './styles';
+
+const CardContent: NextPage<IFoodProps> = ({
+  id,
+  name,
+  description,
+  price,
+  thumbnail,
+  brand,
+  createdAt,
+}) => {
+  const router = useRouter();
+  const [allItems, setAllItems] = useState([]);
+
+  const handleRedirect = () => {
+    router.push(`/produtos/${id}`);
+  };
+
+  useEffect(() => {
+    const formatData = [
+      dayjs(createdAt).format('DD MMM YYYY HH:mm:ss'),
+    ];
+
+    if (router.pathname === '/produtos/todos') {
+      setAllItems([...formatData]);
+    }
+  }, [createdAt, router.pathname]);
+
+  return (
+    <Card>
+      {allItems}
+      {thumbnail ? (
+        <Image
+          src={thumbnail}
+          alt={name}
+          width={200}
+          height={200}
+        />
+      ) : (
+        <Skeleton width={200} height={200} />
+      )}
+      <Title>
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </Title>
+      <Description>
+        <p>{brand}</p>
+        <strong>R$ {price},00</strong>
+      </Description>
+      <Button variant="contained" onClick={handleRedirect}>
+        Editar
+      </Button>
+    </Card>
+  );
+};
+
+export default memo(CardContent);
---
+++ src/components/core/CardContent/styles.ts
@@ -0,0 +1,31 @@
+import { Box, styled } from '@material-ui/core';
+
+export const Card = styled(Box)(() => ({
+  width: 250,
+  height: 300,
+  padding: 15,
+  background: '#fff',
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 10,
+  boxShadow: '0px 0px 10px #00000020',
+}));
+
+export const Title = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'center',
+}));
+
+export const Description = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexDirection: 'row',
+  alignItems: 'center',
+  width: '100%',
+  margin: '10px 0',
+}));
