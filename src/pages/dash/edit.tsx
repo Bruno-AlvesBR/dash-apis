2ba5bb1e5b763a7c5709567785a5e44910880120
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-04-23 18:22:46 -0300

    feat: create first version vision post product

---
+++ src/pages/dash/edit.tsx
@@ -0,0 +1,28 @@
+import { Form } from '../../components/pages/Dash/Form';
+import { UseFood } from '../../hooks/Food';
+import CardContent from '../../components/core/CardContent';
+
+import { Container } from '../../styles/theme';
+
+const index: React.FC = () => {
+  const { productData } = UseFood();
+
+  return (
+    <Container>
+      <Form />
+      <h1>Último produto adicionado</h1>
+      {productData && (
+        <CardContent
+          id={productData?.id}
+          name={productData?.name}
+          description={productData?.description}
+          price={productData?.price}
+          thumbnail={productData?.thumbnail}
+          brand={productData?.brand}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default index;
