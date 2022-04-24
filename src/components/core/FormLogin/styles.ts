c8face6e35ba196c27658ce7d71bf3bdcdd47f5c
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-04-24 00:03:54 -0300

    feat: added button to close moda login

--- src/components/core/FormLogin/styles.ts
+++ src/components/core/FormLogin/styles.ts
@@ -3,11 +3,26 @@ import { styled } from '@material-ui/core';
 export const FormContent = styled('form')(() => ({
   marginTop: 20,
   gap: 20,
+  zIndex: 1000,
 
-  '& button': {
+  '& button:first-child': {
+    paddingg: 5,
+
+    '& svg': {
+      cursor: 'pointer',
+      color: '#6674BB',
+    },
+    '&:hover': {
+      opacity: 1,
+      background: '#aaaaaa60',
+    }
+  },
+
+  '& button:last-child': {
     background: '#6674BB',
     color: '#fff',
     transition: '0.3s all ease',
+
     '&:hover': {
       background: '#6674BB',
       opacity: .6,
