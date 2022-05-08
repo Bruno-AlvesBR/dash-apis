d4ec9f18c4f4f6504e2cf11379a74c46da847a98
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-07 22:29:08 -0300

    feat: created findAll foods service

---
+++ src/services/products/foods/index.ts
@@ -0,0 +1,8 @@
+import { IFoodProps } from "../../../interfaces/IFoodsProps";
+import { api } from "../../api"
+
+export const findAll = async (): Promise<IFoodProps[]> => {
+  const foodResponse = await api.get('product/foods');
+
+  return foodResponse?.data || [];
+}
