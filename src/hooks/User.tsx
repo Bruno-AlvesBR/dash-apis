febd8d77be98e5f69d16e2ca4cbb8bdc23840e30
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-15 00:47:13 -0300

    feat: seting cookie user token, and get him

--- src/hooks/User.tsx
+++ src/hooks/User.tsx
@@ -4,15 +4,13 @@ import {
   useCallback,
   useContext,
   useEffect,
+  useMemo,
   useState,
 } from 'react';
+import Cookies from 'universal-cookie';
 
-import {
-  IUserLogin,
-  IUserProps,
-} from '../interfaces/IUserProps';
+import { IUserLogin, IUserProps } from '../interfaces/IUserProps';
 import { userService } from '../services';
-import { api } from '../services/api';
 
 export interface IUserContext {
   onsubmit?(event: any): void;
@@ -27,29 +25,46 @@ export interface IUserContextProps {
   children: ReactNode;
 }
 
-export const UserContext = createContext(
-  {} as IUserContext,
-);
+export const UserContext = createContext({} as IUserContext);
 
-export const UserContextProvider = ({
-  children,
-}: IUserContextProps) => {
-  const [user, setUser] = useState<IUserProps>();
+export const UserContextProvider = ({ children }: IUserContextProps) => {
+  const [user, setUser] = useState<IUserProps | null>(null);
   const [userId, setUserId] = useState<string>();
   const [noAdmin, setNoAdmin] = useState(false);
   const [isInvalid, setIsInvalid] = useState(false);
-  const [isLoadingUser, setIsLoadingUser] =
-    useState<boolean>();
+  const [isLoadingUser, setIsLoadingUser] = useState<boolean>();
 
-  const onsubmit = useCallback((event: IUserLogin) => {
-    try {
-      setIsLoadingUser(true);
+  const cookie = new Cookies();
 
-      const userResponse = setTimeout(async () => {
+  const userLoadService = useCallback(async () => {
+    const authToken = cookie.get('authUserId');
+
+    if (!authToken) return;
+
+    setUserId(authToken);
+    const userResponse = await userService?.recoveryUser(`${authToken}`);
+
+    if (userResponse) {
+      setUser(userResponse);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    userLoadService();
+  }, [userLoadService]);
+
+  const onsubmit = useCallback(
+    async (event: IUserLogin) => {
+      try {
+        setIsLoadingUser(true);
+
         const userData = await userService.login({
           email: event?.email,
           password: event?.password,
         });
+
+        cookie.set('authUserId', `${userData?.id}`, { maxAge: 60 * 60 * 24 });
 
         if (!userData) return;
 
@@ -59,19 +74,16 @@ export const UserContextProvider = ({
           setUserId(userData?.id);
           setUser(userData);
         }
+      } catch (err) {
+        setIsLoadingUser(false);
+        setIsInvalid(true);
 
-        return () => {
-          clearTimeout(userResponse);
-          setIsLoadingUser(false);
-        };
-      }, 3000);
-    } catch (err) {
-      setIsLoadingUser(false);
-      setIsInvalid(true);
-
-      console.log(err);
-    }
-  }, []);
+        console.log(err);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [user?.id],
+  );
 
   useEffect(() => {
     localStorage.setItem('userName', user?.name?.firstName);
