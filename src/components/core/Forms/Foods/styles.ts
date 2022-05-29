cff430f6412de01cc393240e8c50af4e9873cb34
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-28 23:35:41 -0300

    feat: created form register foods

---
+++ src/components/core/Forms/Foods/styles.ts
@@ -0,0 +1,22 @@
+import { styled } from '@material-ui/core';
+
+export const Container = styled('form')(() => ({
+  zIndex: 99,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  width: '100%',
+  gap: 15,
+
+  '& button': {
+    margin: '10px 0',
+    padding: '10px 20px',
+    width: 'fit-content',
+  },
+
+  '& input': {
+    padding: 20,
+    width: '100%',
+    background: '#fff',
+  },
+}));
