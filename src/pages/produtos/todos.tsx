3d8b1a34dd4d3b1a6044ad63e92283c07f1d5f75
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-06-05 00:39:40 -0300

    feat: import carrousel to list foods

--- src/pages/produtos/todos.tsx
+++ src/pages/produtos/todos.tsx
@@ -2,6 +2,7 @@ import { CircularProgress } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 
 import { CardContent } from '../../components/core/CardContent';
+import Carrousel from '../../components/core/Carrousel';
 import { IFoodProps } from '../../interfaces/IFoodsProps';
 import { foodService } from '../../services';
 
@@ -9,7 +10,6 @@ import Container from './styles';
 
 const Todos = () => {
   const [foodsData, setFoodsData] = useState<IFoodProps[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
 
   const getFoods = () => {
     try {
@@ -19,8 +19,6 @@ const Todos = () => {
         setFoodsData(foodResponse);
         return () => clearTimeout(foodsOnbounce);
       }, 1000);
-
-      setLoading(false);
     } catch (err) {
       console.log(err);
     }
@@ -33,19 +31,21 @@ const Todos = () => {
 
   return (
     <Container>
-      {foodsData && !loading ? (
-        foodsData?.map(food => (
-          <CardContent
-            key={food?.id}
-            id={food?.id}
-            title={food?.title}
-            description={food?.description}
-            image={food?.image}
-            brand={food?.brand}
-          />
-        ))
+      {foodsData.length !== 0 ? (
+        <Carrousel mobileWidth={275} desktopWidth={315}>
+          {foodsData?.map(food => (
+            <CardContent
+              key={food?.id}
+              id={food?.id}
+              title={food?.title}
+              description={food?.description}
+              image={food?.image}
+              brand={food?.brand}
+            />
+          ))}
+        </Carrousel>
       ) : (
-        <CircularProgress style={{ margin: 'auto' }} />
+        <CircularProgress style={{ margin: 10 }} />
       )}
     </Container>
   );
