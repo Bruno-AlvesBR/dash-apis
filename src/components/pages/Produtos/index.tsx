3fc147b8ec8215b2cb94f4011aa590649f42b556
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-06-11 14:51:27 -0300

    feat: created product content to all products page

---
+++ src/components/pages/Produtos/index.tsx
@@ -0,0 +1,61 @@
+import { memo } from 'react';
+
+import { IProductProps } from '../../../interfaces/IProductProps';
+import { CardContent } from '../../core/CardContent';
+import Carrousel from '../../core/Carrousel';
+import { IPodcastProps } from '../../../interfaces/IPodcastProps';
+import { useFood } from '../../../hooks/Product';
+
+import { Container } from './styles';
+
+interface IProductsContentProps {
+  foods: IProductProps[];
+  podcasts: IPodcastProps[];
+}
+
+const ProductsContent: React.FC<IProductsContentProps> = ({
+  foods,
+  podcasts,
+}) => {
+  const [{ setIsLoading }] = [useFood()];
+
+  const FoodContent: React.FC = () => (
+    <Carrousel mobileWidth={275} desktopWidth={315}>
+      {foods?.map(item => (
+        <CardContent
+          key={item?.id}
+          id={item?.id}
+          title={item?.title}
+          description={item?.description}
+          desktopSrc={item?.image?.desktopSrc}
+          createdAt={item?.createdAt}
+          slug={item?.slug}
+        />
+      ))}
+    </Carrousel>
+  );
+
+  const PodcastContent: React.FC = () => (
+    <Carrousel mobileWidth={275} desktopWidth={315}>
+      {podcasts?.map(item => (
+        <CardContent
+          key={item?.id}
+          id={item?.id}
+          title={item?.title}
+          description={item?.description}
+          desktopSrc={item?.thumbnail}
+          createdAt={item?.createdAt}
+        />
+      ))}
+    </Carrousel>
+  );
+
+  return (
+    <Container>
+      <FoodContent />
+      <PodcastContent />
+    </Container>
+  );
+};
+
+export default memo(ProductsContent);
---
+++ src/components/pages/Produtos/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from "@material-ui/core";
+
+export const Container = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%'
+}));
