ad66bd38930696e59ed95a3fb708ef36ccbcb4ef
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-08-05 22:08:40 -0300

    feat: created header component to carrousel

---
+++ src/components/core/Carrousel/Header/styles.ts
@@ -0,0 +1,48 @@
+import { styled } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export const Container = styled(Box)(({ theme }) => ({
+  maxWidth: 'calc(100vw - 240px)',
+  marginBottom: 20,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  background: '#fff',
+  padding: 8,
+  borderRadius: 10,
+
+  [theme.breakpoints.down(780)]: {
+    maxWidth: 'calc(100vw - 45px)',
+  },
+}));
+
+export const ContentButtons = styled(Box)(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '& > svg': {
+    transition: '.2s all ease',
+    padding: 7,
+    width: 40,
+    height: 40,
+    borderRadius: 5,
+    cursor: 'pointer',
+
+    '&:hover': {
+      background: '#C4C4C4',
+    },
+  },
+
+  '& > svg:first-of-type': {
+    transform: 'rotate(180deg)',
+    marginRight: 20,
+  },
+}));
+
+export const Title = styled(Typography)(() => ({
+  fontSize: 20,
+  fontWeight: 700,
+  marginRight: 20,
+}));
