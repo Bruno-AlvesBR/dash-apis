30f053f01911019a2eedaaadd3ff3953ab7c00fc
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-08-21 17:06:01 -0300

    feat: adjusted SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @next/next/no-sync-scripts */
 import { Children } from 'react';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import MaterialUISheets from '@mui/styles/ServerStyleSheets';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -15,11 +20,19 @@ export default class MyDocument extends Document<DocumentProps> {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="Dashboard to control general apis."
+          />
+          <meta name="author" content="Bruno Alves" />
           <link
             href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800&family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap"
             rel="stylesheet"
           />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@700&display=swap"
@@ -47,7 +60,9 @@ MyDocument.getInitialProps = async ctx => {
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: App => rest =>
-        sheet.collectStyles(materialUISheets.collect(<App {...rest} />)),
+        sheet.collectStyles(
+          materialUISheets.collect(<App {...rest} />),
+        ),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
